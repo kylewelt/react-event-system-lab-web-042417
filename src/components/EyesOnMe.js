91ab6e2b841ede1ac5ace33840a11ef7d30d4044
kylewelt <kylewelt@umich.edu>
2017-06-27 10:38:44 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+  logFocus = () => {
+    console.log('Good!')
+  }
+
+  logBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render () {
+    return (
+      <button onFocus={this.logFocus} onBlur={this.logBlur}>Button</button>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
-// Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+  logEntry = () => {
+    console.log('Entering password...')
+  }
+
+  render () {
+    return (
+      <form>
+        <label>
+          Password:
+          <input type='password' onKeyUp={this.logEntry}/>
+        </label>
+      </form>
+    )
+  }
+}
+
+export default Keypad
